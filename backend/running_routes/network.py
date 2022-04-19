b29d3d5c8f4fa9f331e8bf2245cfe04bdb331ef9
Billy <billy.tang164@gmail.com>
2022-04-19 22:36:25 +1000

    Add OSMNetwork class

---
+++ backend/running_routes/network.py
@@ -0,0 +1,101 @@
+import networkx as nx
+import osmnx
+from abc import ABC, abstractmethod, abstractproperty
+
+from typing import Dict, List, Optional
+
+
+class NetworkFactory(ABC):
+    @abstractmethod
+    def create(self, start_coordinate: Dict, distance: int, network_type: str) -> None:
+        """Create the network graph"""
+
+    @abstractmethod
+    def path(self, source: int, target: int) -> List:
+        """Returns the path between source and target"""
+
+    @abstractmethod
+    def length(self, source: int, target: int, max_distance: int) -> float:
+        """Returns the length between source and target"""
+
+    @abstractproperty
+    def nodes(self) -> Dict:
+        """Returns node data"""
+
+
+class OSMNetwork(NetworkFactory):
+    """Create a network using OSMnx's api"""
+
+    def __init__(self, **parameters) -> None:
+        self.graph: nx.DiGraph = None
+        self.parameters: Dict = parameters
+
+        self._length: Dict = {}
+        self._path: Dict = {}
+
+    def create(self, start_coordinate: Dict, distance: int, network_type: str = "walk") -> None:
+        """Downloads the graph and truncates any nodes outside `distance` radius
+
+        Args:
+            start_coordinate (Dict): contains the latitude and longitude
+            distance (int): distance in meters
+            network_type (str, optional): network type according to OSM. Defaults to "walk".
+                https://osmnx.readthedocs.io/en/stable/osmnx.html#module-osmnx.graph    
+        """
+        radius = distance/2
+        G = osmnx.graph_from_point(
+            (start_coordinate["lat"], start_coordinate["lng"]),
+            dist=radius,
+            network_type=network_type,
+        )
+        nodes_outside_radius = [
+            node
+            for node, data in G.nodes.data()
+            if osmnx.distance.great_circle_vec(
+                start_coordinate["lat"], start_coordinate["lng"],
+                data["y"], data["x"]
+            ) > radius
+        ]
+        G.remove_nodes_from(nodes_outside_radius)
+        G = osmnx.utils_graph.get_largest_component(G)
+        self.graph = G
+
+    def path(self, source, target) -> List:
+        if not self.graph:
+            raise Exception("Graph has not been created")
+        if source not in self._path:
+            self._calculate_dijkstras(source)
+        if target not in self.graph:
+            raise nx.NodeNotFound(f"{target}")
+
+        return self._path[source][target]
+
+    def length(self, source, target) -> float:
+        if not self.graph:
+            raise Exception("Graph has not been created")
+        if source not in self._length:
+            self._calculate_dijkstras(source)
+        if target not in self.graph:
+            raise nx.NodeNotFound(f"{target}")
+
+        return self._length[source][target]
+
+    def nearest_nodes(self, locations) -> List:
+        osm_nodes = osmnx.distance.nearest_nodes(
+            self.graph,
+            [location["lng"] for location in locations],
+            [location["lat"] for location in locations]
+        )
+        return osm_nodes
+
+    @property
+    def nodes(self):
+        if not self.graph:
+            raise Exception("Graph has not been created")
+        return {node: data for node, data in self.graph.nodes(data=True)}
+
+    def _calculate_dijkstras(self, source):
+        length, path = nx.single_source_dijkstra(
+            self.graph, source, weight="length")
+        self._length[source] = length
+        self._path[source] = path
---
+++ backend/tests/test_network.py
@@ -0,0 +1,79 @@
+import networkx as nx
+import pytest
+
+from running_routes.network import OSMNetwork
+
+
+class TestOSMNetwork:
+    def test_init(self):
+        network = OSMNetwork()
+        assert network.graph == None
+
+    def test_create(self):
+        """Download the network around Melbourne Central"""
+        start_coordinate = {"lat": -37.8102361, "lng": 144.9627652}
+        network = OSMNetwork()
+
+        # No nodes in the requested polygon
+        with pytest.raises(ValueError):
+            network.create(start_coordinate, distance=20, network_type="walk")
+
+        # Four nodes in the bbox around the location
+        # - Three in the bbox and radius: 2384426953, 6806666960, 6806666961, 6806666963
+        # - One in the bbox and not in the radius: 176693380
+        network.create(start_coordinate, distance=100, network_type="walk")
+        nodes_in = [2384426953, 6806666960, 6806666961, 6806666963]
+        assert nodes_in == sorted(network.nodes.keys())
+        assert 176693380 not in network.nodes.keys()
+
+    def test_path(self):
+        start_coordinate = {"lat": -37.8102361, "lng": 144.9627652}
+        distance = 100
+        network = OSMNetwork()
+
+        # Need to call `create`
+        with pytest.raises(Exception):
+            network.path(2384426953, 6806666960)
+
+        # Check for invalid arguments
+        network.create(start_coordinate, distance)
+        with pytest.raises(nx.NodeNotFound):
+            network.path("invalid", 2384426953)
+        with pytest.raises(nx.NodeNotFound):
+            network.path(2384426953, "invalid")
+
+        # Validate correctness
+        assert [2384426953, 6806666960] == network.path(2384426953, 6806666960)
+
+    def test_distance(self):
+        start_coordinate = {"lat": -37.8102361, "lng": 144.9627652}
+        distance = 100
+        network = OSMNetwork()
+
+        # Need to call `create`
+        with pytest.raises(Exception):
+            network.length(2384426953, 6806666960)
+
+        # Check for invalid arguments
+        network.create(start_coordinate, distance)
+        with pytest.raises(nx.NodeNotFound):
+            network.length("invalid", 2384426953)
+        with pytest.raises(nx.NodeNotFound):
+            network.length(2384426953, "invalid")
+
+        # Validate correctness
+        # ~31.264m
+        assert 31 <= network.length(2384426953, 6806666960) <= 32
+
+    def test_nodes(self):
+        with pytest.raises(Exception):
+            network = OSMNetwork()
+            network.nodes
+
+    def test_nearest_nodes(self):
+        start_coordinate = {"lat": -37.8102361, "lng": 144.9627652}
+        distance = 100
+
+        network = OSMNetwork()
+        network.create(start_coordinate, distance)
+        assert network.nearest_nodes([start_coordinate]) == [6806666961]
