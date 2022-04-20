98a8fc358440d57740ea4732a2fe55c1997ea967
Billy <billy.tang164@gmail.com>
2022-04-20 22:18:21 +1000

    Add VRPModel

---
+++ backend/running_routes/model.py
@@ -0,0 +1,188 @@
+from abc import ABC, abstractmethod
+import itertools
+
+import networkx as nx
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+from sklearn.cluster import KMeans
+
+from running_routes.abstract_classes import NetworkFactory, ModelFactory
+
+from typing import Dict, List, Tuple
+
+VRP_DEFAULT_PARAMETERS = {
+    "sample_percent": 0.2,
+    "seed": 1234,
+    "time_limit": 10
+}
+
+
+class ModelFactory(ABC):
+    """Factory that represents different model implementations"""
+    @abstractmethod
+    def __init__(self, n, distance, **kwargs):
+        """Load parameters"""
+
+    @abstractmethod
+    def solve(self, location: Dict, network: NetworkFactory) -> List[List]:
+        """Creates and solves the model"""
+
+
+class VRPModel(ModelFactory):
+    """
+    Implements a vehicle routing problem based model through
+    constraint programming and OR-tools
+
+    https://developers.google.com/optimization/routing/vrp
+    https://developers.google.com/optimization/routing/penalties
+    """
+
+    def __init__(self, **parameters):
+        self.parameters = parameters
+
+        # Set default parameters
+        for key, value in VRP_DEFAULT_PARAMETERS.items():
+            if key not in self.parameters:
+                self.parameters[key] = value
+
+    def solve(self, n: int, distance: int, start_coordinate: Dict, network: NetworkFactory) -> List[List]:
+        sample_coordinates = self._downsample(
+            network, self.parameters["sample_percent"], self.parameters["seed"])
+        sample_nodes = self._find_sample_nodes(start_coordinate, sample_coordinates, network)
+        distance_matrix = self._construct_distance_matrix(sample_nodes, network)
+        manager, routing = self._construct_cp_model(n, distance, distance_matrix)
+        assignment = self._solve_cp_model(
+            routing, self.parameters["time_limit"])
+        results = self._generate_results(
+            n, sample_nodes, network,
+            routing, manager,
+            assignment)
+        return results
+
+    def _downsample(self, network: NetworkFactory, sample_percent: float, seed: int) -> List[Dict]:
+        """Downsample using KMeans"""
+        coordinates = [
+            [data["y"], data["x"]]
+            for _, data in network.nodes.items()
+        ]
+        sample_size = int(len(coordinates)*sample_percent)
+        kmeans = KMeans(n_clusters=sample_size,
+                        random_state=seed).fit(coordinates)
+        sample_coordinates = [
+            {"lat": center[0], "lng": center[1]}
+            for center in kmeans.cluster_centers_
+        ]
+        return sample_coordinates
+
+    def _find_sample_nodes(
+                self, start_coordinate: Dict, sample_coordinates: List[Dict],
+                network: NetworkFactory) -> List:
+        # `location` is the zeroth element by construction
+        # Remove any duplicate nodes
+        # https://stackoverflow.com/a/17016257
+        model_coordinates = [start_coordinate] + sample_coordinates
+        nearest_nodes = network.nearest_nodes(model_coordinates)
+        sample_nodes = list(dict.fromkeys(nearest_nodes))
+        return sample_nodes
+
+    def _construct_distance_matrix(self, sample_nodes: List, network: NetworkFactory) -> List[List[float]]:
+        # If there are no path between source and target, return `self.total_length`
+        distance_matrix = []
+        for source in sample_nodes:
+            distance_from_source = []
+            for target in sample_nodes:
+                try:
+                    length = network.length(source, target)
+                except nx.NodeNotFound:
+                    length = self.distance
+                distance_from_source.append(length)
+            distance_matrix.append(distance_from_source)
+
+        return distance_matrix
+
+    def _construct_cp_model(
+            self, n: int, distance: int,
+            distance_matrix: List[List],
+    ) -> Tuple[pywrapcp.RoutingIndexManager, pywrapcp.RoutingModel]:
+        """
+        Follows the vrp model with drop penalties
+        https://developers.google.com/optimization/routing/vrp
+        """
+        if not distance_matrix:
+            raise Exception("Invalid distance_matrix")
+
+        # The depot is the zeroth element by construction
+        manager = pywrapcp.RoutingIndexManager(
+            len(distance_matrix), n, 0)
+        routing = pywrapcp.RoutingModel(manager)
+
+        def distance_callback(from_index, to_index):
+            """
+            Create and register transit callback
+            Returns the distance between the two nodes
+            """
+            from_node = manager.IndexToNode(from_index)
+            to_node = manager.IndexToNode(to_index)
+            return distance_matrix[from_node][to_node]
+        transit_callback_index = routing.RegisterTransitCallback(
+            distance_callback)
+
+        routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+        dimension_name = "distance"
+        routing.AddDimension(
+            transit_callback_index, 0, distance, True, dimension_name)
+
+        # Add the ability to drop nodes for a penalty
+        drop_penalty = n*distance
+        for node in range(1, len(distance_matrix)):
+            routing.AddDisjunction([manager.NodeToIndex(node)], drop_penalty)
+
+        return manager, routing
+
+    def _solve_cp_model(
+            self, routing: pywrapcp.RoutingIndexManager, time_limit: float
+    ) -> pywrapcp.Assignment:
+        # Use the suggested search strategy
+        search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+        search_parameters.first_solution_strategy = (
+            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+        search_parameters.local_search_metaheuristic = (
+            routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
+        search_parameters.time_limit.seconds = time_limit
+
+        assignment = routing.SolveWithParameters(search_parameters)
+        return assignment
+
+    def _generate_results(
+            self,
+            n: int,
+            candidate_nodes: List,
+            network: NetworkFactory,
+            routing: pywrapcp.RoutingIndexManager,
+            manager: pywrapcp.RoutingModel,
+            assignment: pywrapcp.Assignment) -> List[List]:
+        """Returns the paths in the context of network nodes"""
+        results = []
+
+        for route_id in range(n):
+            index = routing.Start(route_id)
+            candidate_tour = []
+            single_route = []
+
+            # Extract the tour from candidate nodes and cp model
+            while not routing.IsEnd(index):
+                node_index = manager.IndexToNode(index)
+                candidate_tour.append(candidate_nodes[node_index])
+                index = assignment.Value(routing.NextVar(index))
+            node_index = manager.IndexToNode(index)
+            candidate_tour.append(candidate_nodes[node_index])
+
+            # Join the tour with the shortest paths
+            for source, target in zip(candidate_tour, candidate_tour[1:]):
+                single_route.extend(network.path(source, target))
+            single_route += [single_route[0]]
+
+            # Remove any duplicate nodes in the route
+            dedup_nodes = [group[0] for group in itertools.groupby(single_route)]
+            results.append(dedup_nodes)
+        return results
---
+++ backend/tests/test_model.py
@@ -0,0 +1,76 @@
+import pytest
+
+from running_routes.network import OSMNetwork
+from running_routes.model import VRPModel
+
+@pytest.fixture
+def n():
+    return 1
+
+@pytest.fixture
+def start_coordinate():
+    return {"lat": -37.8102361, "lng": 144.9627652}
+
+
+@pytest.fixture
+def sample_percent():
+    return 1
+
+
+@pytest.fixture
+def distance():
+    return 100
+
+
+@pytest.fixture
+def network(start_coordinate, distance):
+    network_instance = OSMNetwork()
+    network_instance.create(start_coordinate, distance)
+    return network_instance
+
+
+@pytest.fixture
+def model(sample_percent):
+    return VRPModel(sample_percent=sample_percent, time_limit=2)
+
+
+class TestVRPModel:
+    def test_solve(self, n, distance, model, start_coordinate, network) -> None:
+        test_results = [[6806666961, 6806666963, 6806666961]]
+        results = model.solve(n, distance, start_coordinate, network)
+        assert results == test_results
+
+    def test__downsample(self, model, network) -> None:
+        # setting sample_percent to 1 will return all nodes in the network
+        sample_percent = 1
+        seed = 1234
+        sample_coordinates = [
+            {'lat': -37.8104663, 'lng': 144.962858},
+            {'lat': -37.8100645, 'lng': 144.9626779},
+            {'lat': -37.810335, 'lng': 144.9632837},
+            {'lat': -37.8104393, 'lng': 144.9629538},
+        ]
+        assert model._downsample(
+            network, sample_percent, seed) == sample_coordinates
+
+    def test__find_sample_nodes(self, model, start_coordinate, network) -> None:
+        sample_nodes= [6806666961, 6806666963, 2384426953, 6806666960]
+        sample_coordinates = [
+            {'lat': -37.8104663, 'lng': 144.962858},
+            {'lat': -37.8100645, 'lng': 144.9626779},
+            {'lat': -37.810335, 'lng': 144.9632837},
+            {'lat': -37.8104393, 'lng': 144.9629538},
+        ]
+        assert model._find_sample_nodes(
+            start_coordinate, sample_coordinates, network) == sample_nodes
+
+    def test__construct_distance_matrix(self, model, network) -> None:
+        sample_nodes= [6806666961, 6806666963, 2384426953, 6806666960]
+        distance_matrix = [
+            [0, 47.397, 87.596, 56.332],
+            [47.397, 0, 40.199000000000005, 8.935],
+            [87.596, 40.199000000000005, 0, 31.264000000000003],
+            [56.332, 8.935, 31.264000000000003, 0]
+        ]
+        assert model._construct_distance_matrix(
+            sample_nodes, network) == distance_matrix
