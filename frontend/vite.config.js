baace7bb5a2fe7f48d916753154c80f1784666e2
Billy <billy.tang164@gmail.com>
2022-05-15 01:06:10 +1000

    Comment out base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/running-routes/',
+  //base: '/running-routes/',
   plugins: [vue()],
   resolve: {
     alias: {
