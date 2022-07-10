3c7c0bf9b7d629c53b977de4340641dd707f3691
Billy <billy.tang164@gmail.com>
2022-07-10 20:54:10 +1000

    Feature: Update backtrack elimination local search to support non-adjacent nodes in the tour

--- backend/running_routes/local_search.py
+++ backend/running_routes/local_search.py
@@ -34,26 +34,21 @@ class BacktrackEliminationLocalSearch(LocalSearchFactory):
     def iterate(
             self, tours: List[List], distance: int, network: NetworkFactory) -> List[List]:
         output_tours = []
+        start_node = tours[0][0]
         for tour in tours:
-            G = nx.Graph()
-            G.add_edges_from([(x, y) for x, y in zip(tour, tour[1:])])
-            node_degrees = {node: degree for (node, degree) in G.degree()}
-            single_degree_nodes = [
-                node for node, degree in node_degrees.items() if degree == 1 and node != tour[0]]
-            three_or_more_degree_nodes = [
-                node for node, degree in node_degrees.items() if degree > 2]
+            backtrack_elimination_tour = []
+            piecewise_paths = []
+            for source, target in zip(tour, tour[1:]):
+                piecewise_paths.append(network.path(source, target))
             
-            if not len(three_or_more_degree_nodes):
-                output_tours.append(tour)
-            else:
-                for single_degree in single_degree_nodes:
-                    shortest_paths = nx.single_source_shortest_path(G, single_degree)
-                    candidate_paths = [shortest_paths[node] for node in three_or_more_degree_nodes]
-                    backtrack_path = sorted(candidate_paths, key=len)[0]
-                    G.remove_nodes_from(backtrack_path[:-1])
-                output_tour = [tour[0]]
-                for node in tour[1:]:
-                    if node in G and G.degree[node] > 0 and node != output_tour[-1]:
-                        output_tour.append(node)
-                output_tours.append(output_tour)
+            for path1, path2 in zip(piecewise_paths, piecewise_paths[1:]):
+                try:
+                    first_divergence_index = next(
+                        index 
+                        for index, (node1, node2) in enumerate(zip(path1[::-1], path2))
+                        if node1 != node2)
+                except StopIteration:
+                    first_divergence_index = 1
+                backtrack_elimination_tour.append(path2[first_divergence_index-1])
+            output_tours.append(backtrack_elimination_tour + [start_node]) 
         return output_tours
